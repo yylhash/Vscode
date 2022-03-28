475c9b57b1e9f3a210758382c01f686dda83abd3
yylhash <229552781@qq.com>
2022-03-28 22:51:08 +0800

    master

--- New Computer Build/Text/text.java
+++ New Computer Build/Text/text.java
@@ -1,5 +1,11 @@
-public class Text {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+import java.util.regex.*;
+ 
+class Text{
+   public static void main(String[] args){
+      String content = "I am noob " +"from runoob.com.";
+      String pattern = ".*runoob.*";
+ 
+      boolean isMatch = Pattern.matches(pattern, content);
+      System.out.println("字符串中是否包含了 'runoob' 子字符串? " + isMatch);
+   }
 }
