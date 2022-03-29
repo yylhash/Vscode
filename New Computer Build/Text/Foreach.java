f74b533c88db2cf1bf5b687d34adc9b0abfbeb25
yylhash <229552781@qq.com>
2022-03-29 15:28:54 +0800

    master

---
+++ New Computer Build/Text/Foreach.java
@@ -0,0 +1,11 @@
+public class Foreach {
+    public static void main(String[] args) {
+        String[] urls = { "http://c.biancheng.net/java", "http://c.biancheng.net/c", "http://c.biancheng.net/golang/" };
+// 使用foreach循环来遍历数组元素
+// 其中book将会自动迭代每个数组元素
+for (String url : urls) {
+    System.out.println(url);
+}
+
+    }
+}
---
+++ New Computer Build/Text/Haskell.java
@@ -0,0 +1,13 @@
+public class Haskell{
+    public static void main(String[] args) {
+        System.out.println("乘法口诀表：");
+    // 外层循环
+    for (int i = 1; i <= 9; i++) {
+        // 内层循环
+        for (int j = 1; j <= i; j++) {
+            System.out.print(j + "*" + i + "=" + j * i + "\t");
+        }
+        System.out.println();
+    }
+    }
+}
