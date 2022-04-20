e35d2b42ec8da9e4227d888035016bbf696596a1
yylhash <229552781@qq.com>
2022-04-20 10:36:22 +0800

    master

--- New Computer Build/Text/Test.java
+++ New Computer Build/Text/Test.java
@@ -1,15 +1,6 @@
-import java.util.Date;
-
-
 public class Test {
     public static void main(String[] args) {
-        try { 
-            Date time1 = new Date();
-            System.out.println(time1);
-            Thread.sleep(1000*3);   // 休眠3秒
-            System.out.println(new Date() + "\n"); 
-         } catch (Exception e) { 
-             System.out.println("Got an exception!"); 
-         }
+        int[] arr = new int[9];
+        System.out.println(arr[9]);
     }
 }
---
+++ New Computer Build/Text/twoArray.java
@@ -0,0 +1,24 @@
+public class twoArray{
+    public static void main(String[] args) {
+        int[][] intArray = new int[10][10];
+        for(int i = 0;i < intArray.length;i++){
+            for(int j = 0;j < intArray[i].length;j++){
+                intArray[i][j] = (int)(Math.random() * 10);
+            }
+        }
+
+        for(int[] arrayList : intArray){//for-each循环
+            for(int a : arrayList){
+                System.out.print(a + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println("**********");
+        for(int i = 0;i < intArray.length;i++){
+            for(int j = 0;j < intArray[i].length;j++){
+                System.out.print(intArray[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
