d8cab4fa23f58681fc7f52fb9edc0f59e2596134
yylhash <229552781@qq.com>
2022-04-27 22:54:54 +0800

    master

--- New Computer Build/Text/Others/Test.java
+++ New Computer Build/Text/Others/Test.java
@@ -5,11 +5,21 @@ public class Test {
     public String longestCommonPrefix(String[] strs) {
         String str = "",strC = "";
         int len = strs.length;
+        int min = strs[0].length();
         String[][] strCopy = new String[len][];
+
+        if(len == 1) {
+            return strC = strs[0];
+        }
         for (int i = 0; i < len; i++) {
             strCopy[i] = strs[i].split("");
         }
-        for (int j = 0; j < strCopy[j].length; j++) {// 进行列循环
+        for(int i = 0; i< len; i++){
+            if(strs[i].length() <= min) {
+                min = strs[i].length();
+            } 
+        }
+        for (int j = 0; j < min; j++) {// 进行列循环
             for (int i = 0; i < len - 1; i++) { // 进行行循环
                 if (strCopy[i][j].equals(strCopy[i + 1][j])) {
                     str = strCopy[i][j];
@@ -28,7 +38,7 @@ public class Test {
     }
 
     public static void main(String[] args) {
-        String[] strs = { "flower", "flow", "flight" };
+        String[] strs = { "ab", "a"};
         Test test = new Test();
         System.out.println(test.longestCommonPrefix(strs));
     }
